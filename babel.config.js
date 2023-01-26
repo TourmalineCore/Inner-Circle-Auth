b0df75fdb7d38e502ff902c296a219af86eee83f
Iliandr123 <isapronov@tourmalinecore.com>
2023-01-26 16:07:01 +0500

    feat(jest): add jest for snapshot testing

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+};
+  // ],
+  // plugins: [
+    // "transform-es2015-modules-commonjs",
+    // "@babel/plugin-transform-modules-commonjs",
+    // 'add-module-exports',
+  // ],
+// {
+  // "plugins": ["@babel/plugin-transform-modules-commonjs"],
+// }
---
+++ jest.config.js
@@ -0,0 +1,31 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  testEnvironment: 'jsdom',
+  preset: 'ts-jest',
+  verbose: true,
+  testMatch: [
+    '<rootDir>/src/**/**.test.[j|t]sx',
+  ],
+  moduleFileExtensions: [
+    'js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx', 'json', 'node',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    //
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+  ],
+  collectCoverage: false,
+  moduleNameMapper: {
+    '^.+\\.(css|scss)$': '<rootDir>/config/CSSStub.js',
+  },
+  setupFiles: [
+    '<rootDir>/public/env-config.js',
+    '<rootDir>/src/mocks/intersectionObserverMock.js',
+  ],
+};
